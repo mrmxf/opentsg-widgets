6e35e9d99bccd4ecc16afd46c49f3157c72c9426
Tristan Larke <tristan@mrmxf.com>
2023-10-10 15:25:35 +0000

    started ramps test suite

--- ramps/ramps_test.go
+++ ramps/ramps_test.go
@@ -1,69 +1,156 @@
 package ramps
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"image"
+	"image/draw"
 	"image/png"
 	"os"
 	"testing"
 
 	examplejson "github.com/mrmxf/opentsg-widgets/exampleJson"
-	// . "github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestTemp(t *testing.T) {
 	mock := Ramp{Groups: []RampProperties{{Colour: "green", InitialPixelValue: 960}, {Colour: "gray", InitialPixelValue: 960}},
 		Gradients: groupContents{GroupSeparator: groupSeparator{Height: 0, Colour: "white"},
 			GradientSeparator: gradientSeparator{Colours: []string{"white", "black", "red", "blue"}, Height: 1},
-			Gradients:         []Gradient{{Height: 5, BitDepth: 4, Label: "ブルースのテスト列"}, {Height: 5, BitDepth: 6, Label: "ब्रूस परीक्षण पंक्ति"}, {Height: 5, BitDepth: 8, Label: "8b"}, {Height: 5, BitDepth: 10, Label: "10b"}}},
+			Gradients:         []Gradient{{Height: 5, BitDepth: 4, Label: "4b"}, {Height: 5, BitDepth: 6, Label: "6b"}, {Height: 5, BitDepth: 8, Label: "8b"}, {Height: 5, BitDepth: 10, Label: "10b"}}},
 		WidgetProperties: control{MaxBitDepth: 10, TextProperties: textObjectJSON{TextColour: "#345AB6", TextHeight: 70}}}
 	tester := image.NewNRGBA64(image.Rect(0, 0, 1024, 1000)) //960))
 	firstrun(tester, mock)
 
 	examplejson.SaveExampleJson(mock, "builtin.ramps2", "demo")
 
-	f, _ := os.Create("tester.png")
+	f, _ := os.Create("./testdata/tester.png")
 	png.Encode(f, tester)
 
-	rotates := []string{"π*1/2", "π*3/2", "π*2/2", "π*5/8"}
-	names := []string{"tester90.png", "tester270.png", "tester180.png", "testerwonk.png"}
+	/*
+		rotates := []string{"π*1/2", "π*3/2", "π*2/2", "π*5/8"}
+		names := []string{"tester90.png", "tester270.png", "tester180.png", "testerwonk.png"}
 
-	for i, ang := range rotates {
-		mockAngle := mock
-		mockAngle.WidgetProperties.CwRotation = ang
-		// mock.WidgetProperties.TextProperties = textObjectJSON{TextColour: "#F32399"}
+		for i, ang := range rotates {
+			mockAngle := mock
+			mockAngle.WidgetProperties.CwRotation = ang
+			// mock.WidgetProperties.TextProperties = textObjectJSON{TextColour: "#F32399"}
 
-		testerAng := image.NewNRGBA64(image.Rect(0, 0, 4000, 4000))
-		firstrun(testerAng, mockAngle)
+			testerAng := image.NewNRGBA64(image.Rect(0, 0, 4000, 4000))
+			firstrun(testerAng, mockAngle)
 
-		fang, _ := os.Create(names[i])
-		png.Encode(fang, testerAng)
-	}
+			fang, _ := os.Create(names[i])
+			png.Encode(fang, testerAng)
+		}
 
-	mock.Groups = []RampProperties{{Colour: "gray", InitialPixelValue: 1023, Reverse: true}}
-	mock.WidgetProperties = control{MaxBitDepth: 10, TextProperties: textObjectJSON{TextColour: "#F32399"}}
-	mock.Gradients.Gradients = []Gradient{{Height: 20, BitDepth: 8}, {Height: 20, BitDepth: 4}}
-	tester2 := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000))
-	firstrun(tester2, mock)
+		mock.Groups = []RampProperties{{Colour: "gray", InitialPixelValue: 1023, Reverse: true}}
+		mock.WidgetProperties = control{MaxBitDepth: 10, TextProperties: textObjectJSON{TextColour: "#F32399"}}
+		mock.Gradients.Gradients = []Gradient{{Height: 20, BitDepth: 8}, {Height: 20, BitDepth: 4}}
+		tester2 := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000))
+		firstrun(tester2, mock)
 
-	f2, _ := os.Create("tester2.png")
-	png.Encode(f2, tester2)
+		f2, _ := os.Create("tester2.png")
+		png.Encode(f2, tester2)
 
-	mock = Ramp{Groups: []RampProperties{{Colour: "green", InitialPixelValue: 960}, {Colour: "gray", InitialPixelValue: 960}},
+		mock = Ramp{Groups: []RampProperties{{Colour: "green", InitialPixelValue: 960}, {Colour: "gray", InitialPixelValue: 960}},
+			Gradients: groupContents{GroupSeparator: groupSeparator{Height: 0, Colour: "white"},
+				GradientSeparator: gradientSeparator{Colours: []string{"white", "black", "red", "blue"}, Height: 1},
+				Gradients:         []Gradient{{Height: 5, BitDepth: 4, Label: "4b"}, {Height: 5, BitDepth: 6, Label: "6b"}, {Height: 5, BitDepth: 8, Label: "8b"}, {Height: 5, BitDepth: 10, Label: "10b"}}},
+			WidgetProperties: control{MaxBitDepth: 10, TextProperties: textObjectJSON{TextColour: "#345AB6", TextHeight: 70}}}
+		Squeezer := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000)) //960))
+		mock.WidgetProperties.ObjectFitFill = true
+		firstrun(Squeezer, mock)
+		fsqueeze, _ := os.Create("testerSqu.png")
+		png.Encode(fsqueeze, Squeezer)
+
+		twoer := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000)) //960))
+		mock.WidgetProperties.ObjectFitFill = false
+		mock.WidgetProperties.PixelValueRepeat = 2
+		firstrun(twoer, mock)
+		fstwo, _ := os.Create("testerTwo.png")
+		png.Encode(fstwo, twoer)
+	*/
+}
+
+func TestRotation(t *testing.T) {
+
+	mock := Ramp{Groups: []RampProperties{{Colour: "green", InitialPixelValue: 960}, {Colour: "gray", InitialPixelValue: 960}},
 		Gradients: groupContents{GroupSeparator: groupSeparator{Height: 0, Colour: "white"},
 			GradientSeparator: gradientSeparator{Colours: []string{"white", "black", "red", "blue"}, Height: 1},
 			Gradients:         []Gradient{{Height: 5, BitDepth: 4, Label: "4b"}, {Height: 5, BitDepth: 6, Label: "6b"}, {Height: 5, BitDepth: 8, Label: "8b"}, {Height: 5, BitDepth: 10, Label: "10b"}}},
 		WidgetProperties: control{MaxBitDepth: 10, TextProperties: textObjectJSON{TextColour: "#345AB6", TextHeight: 70}}}
-	Squeezer := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000)) //960))
-	mock.WidgetProperties.ObjectFitFill = true
-	firstrun(Squeezer, mock)
-	fsqueeze, _ := os.Create("testerSqu.png")
-	png.Encode(fsqueeze, Squeezer)
 
-	twoer := image.NewNRGBA64(image.Rect(0, 0, 5000, 1000)) //960))
-	mock.WidgetProperties.ObjectFitFill = false
-	mock.WidgetProperties.PixelValueRepeat = 2
-	firstrun(twoer, mock)
-	fstwo, _ := os.Create("testerTwo.png")
-	png.Encode(fstwo, twoer)
+	explanationRight := []string{"flat", "90degrees", "180degrees", "270degrees"}
+	anglesRight := []string{"", "π*1/2", "π*1", "π*3/2"}
+	testFRight := []string{"./testdata/test.png", "./testdata/test90.png", "./testdata/test180.png", "./testdata/test270.png"}
 
+	for i, angle := range anglesRight {
+
+		mock.WidgetProperties.CwRotation = angle
+
+		angleImage := image.NewNRGBA64(image.Rectangle{image.Point{0, 0}, image.Point{4096, 2000}})
+		examplejson.SaveExampleJson(mock, "builtin.ramps2", explanationRight[i])
+		genErr := firstrun(angleImage, mock)
+		// Generate the ramp image
+		// genErr := mock.Generate(myImage)
+		// Open the image to compare to
+		file, _ := os.Open(testFRight[i])
+		// Decode to get the colour values
+		baseVals, _ := png.Decode(file)
+		// Assign the colour to the correct type of image NGRBA64 and replace the colour values
+		readImage := image.NewNRGBA64(baseVals.Bounds())
+		draw.Draw(readImage, readImage.Bounds(), baseVals, image.Point{0, 0}, draw.Over)
+		png.Encode(file, angleImage)
+		// Make a hash of the pixels of each image
+		hnormal := sha256.New()
+		htest := sha256.New()
+		hnormal.Write(readImage.Pix)
+		htest.Write(angleImage.Pix)
+		// f, _ := os.Create(testF[i] + fmt.Sprintf("%v.png", i))
+		// png.Encode(f, myImage)
+
+		Convey("Checking the ramps are generated at 90 degree angles", t, func() {
+			Convey(fmt.Sprintf("Comparing the generated ramp to %v with an angle of %v", testFRight[i], angle), func() {
+				Convey("No error is returned and the file matches", func() {
+					So(genErr, ShouldBeNil)
+					So(htest.Sum(nil), ShouldResemble, hnormal.Sum(nil))
+				})
+			})
+		})
+	}
+
+	anglesOffRight := []string{"π*1/20", "π*5/12", "π*9/10", "π*31/20"}
+	explanation := []string{"9degrees", "75degrees", "162degrees", "279degrees"}
+	testFRightOff := []string{"./testdata/angLinear.png", "./testdata/ang90.png", "./testdata/ang180.png", "./testdata/ang270.png"}
+
+	for i, angle := range anglesOffRight {
+		mock.WidgetProperties.CwRotation = angle
+		angleImage := image.NewNRGBA64(image.Rectangle{image.Point{0, 0}, image.Point{4096, 2000}})
+		examplejson.SaveExampleJson(mock, "builtin.ramps2", explanation[i])
+		// Generate the ramp image
+		genErr := firstrun(angleImage, mock)
+		// Open the image to compare to
+		file, _ := os.Open(testFRightOff[i])
+
+		png.Encode(file, angleImage)
+		// Decode to get the colour values
+		baseVals, _ := png.Decode(file)
+		// Assign the colour to the correct type of image NGRBA64 and replace the colour values
+		readImage := image.NewNRGBA64(baseVals.Bounds())
+		draw.Draw(readImage, readImage.Bounds(), baseVals, image.Point{0, 0}, draw.Over)
+
+		// Make a hash of the pixels of each image
+		hnormal := sha256.New()
+		htest := sha256.New()
+		hnormal.Write(readImage.Pix)
+		htest.Write(angleImage.Pix)
+		Convey("Checking the ramps are generated at angles other than 90 degrees", t, func() {
+			Convey(fmt.Sprintf("Comparing the generated ramp to %v with an angle of %v", testFRightOff[i], angle), func() {
+				Convey("No error is returned and the file matches", func() {
+					So(genErr, ShouldBeNil)
+					So(htest.Sum(nil), ShouldResemble, hnormal.Sum(nil))
+				})
+			})
+		})
+	}
 }
